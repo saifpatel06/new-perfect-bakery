e6750f2cc6e6b504a39bdd0893752798f34590b2
saifpatel06 <saifradiocity@gmail.com>
2026-06-22 14:08:17 +0530

    Enhance styles across multiple components including Bulk Order Section, Footer, Hero, Inquiry Form, and Product Card for improved aesthetics and responsiveness

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,10 @@
 import "@/styles/globals.css";
 
-import "@fontsource/playfair-display";
-import "@fontsource/plus-jakarta-sans";
+import "@fontsource/playfair-display/700.css";
+import "@fontsource/plus-jakarta-sans/400.css";
+import "@fontsource/plus-jakarta-sans/500.css";
+import "@fontsource/plus-jakarta-sans/600.css";
+import "@fontsource/plus-jakarta-sans/700.css";
 
 export default function App({
   Component,
